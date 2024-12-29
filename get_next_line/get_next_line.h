43c1c23e634f632d7bd5eaf9fb90e83753e7ef02
Talha <mutlutalha02@gmail.com>
2024-12-29 20:43:38 +0100

    added files to repo

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tamutlu <tamutlu@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/29 18:36:54 by tamutlu           #+#    #+#             */
+/*   Updated: 2024/12/29 18:43:36 by tamutlu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
